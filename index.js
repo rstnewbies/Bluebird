7495edc256287aa50fb91f0aa046f47fa99ef108
Mateusz <Mateusz>
2016-04-25 21:03:00 +0200

    add logo in top, add generator for random user id, create directory named public

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+app.use(express.static('public'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
