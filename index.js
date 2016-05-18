2c83aab813acbfb53d51a42a9875cdbc69a0ed49
Newbies <newbies@rst.com.pl>
2016-05-18 21:09:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.sendFile(__dirname + '/index.html');
 });
 /*io.on('connection', function(socket){
   socket.on('chat message', function(msg){
