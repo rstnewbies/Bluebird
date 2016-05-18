15eda48dacaee330f5b9777f72b5a5fca6bb96c8
Newbies <newbies@rst.com.pl>
2016-05-18 22:32:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
 
 
 var io = require('socket.io')(http);
