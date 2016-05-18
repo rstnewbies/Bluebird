6b56b8ee5da4536f1161563d1226707ac70067db
Newbies <newbies@rst.com.pl>
2016-05-18 21:59:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
-
-app.use(express.static(__dirname + "/public"))
-
-var server = http.createServer(app)
-server.listen(port)
-
-console.log("http server listening on %d", port)
-
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on('chat message', function(msg){
-    ws.emit('chat message', msg);
-  });
-  
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
-
-
-
-
-
-/*var express = require('express');
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
@@ -64,7 +27,7 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-*/
+
 
 
 
