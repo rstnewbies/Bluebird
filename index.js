10709a2c3075126d392881c4e2ab46ef1c2258d9
Mesiak <mesq721@wp.pl>
2016-05-22 15:44:29 +0200

    #6 make username bold

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-/*var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-*/
 var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
-/*var io = require('socket.io')(http);
-io.set('transports', ['xhr-polling']);
-io.set('polling duration', 10);*/
-
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
@@ -31,26 +16,3 @@ server.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
-
-
-
-
-/*var express = require('express');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 5000;
-
-app.use(express.static('public'));
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-http.listen(port, function(){
-  console.log('listening on *:3000');
-});*/
