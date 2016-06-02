e333ac27627562ef1649350bb0c9cf2839f47b30
Mesiak <mesq721@wp.pl>
2016-06-02 16:04:27 +0200

    #12 add colon after nickname, small refactor

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var data = {};
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('message to server', function(msg){
+    io.emit('message from server', msg);
   });
 });
 server.listen(app.get('port'), function() {
