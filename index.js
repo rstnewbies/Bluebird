6c1ab1c644adce8670cf674d6de9a5af35a41ec5
Mesiak <mesq721@wp.pl>
2016-06-04 13:28:09 +0200

    #9 user can't have empty name

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var data = {};
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function(socket){
+	/**
+	 * When user send message
+	 */
+	
   socket.on('message to server', function(msg){
     io.emit('message from server', msg);
   });
-  
   /**
    * When new user joins the chat
    */
